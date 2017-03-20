9ea9b62e9bc44c6409dfd6af43b89212e6350c17
Bhavik Gosrani <Bhavik Gosrani>
2017-03-19 22:57:34 -0400

    add angular-boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+	AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
